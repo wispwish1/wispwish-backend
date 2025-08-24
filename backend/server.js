4e16093767c6eb60ada3ae6859c8ec806fb6e990
wispwish1 <1wispwish@gmail.com>
2025-08-24 14:48:51 +0500

    Updated backend with latest changes cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5500'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:5500' ,'http://www.wispwish.com'],
     credentials: true,
 }));
 
