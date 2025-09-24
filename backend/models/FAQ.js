e4d32408cacd86dc5c6d8e20946771f64a85e42a
wispwish <1wispwish@gmail.com>
2025-09-24 18:54:12 +0500

    Delete backend directory

--- backend/models/FAQ.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const FAQSchema = new mongoose.Schema({
-  question: { type: String, required: true },
-  answer: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('FAQ', FAQSchema);
