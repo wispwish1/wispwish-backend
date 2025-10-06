e97b8a8f453bc01f54135818933b1bef95c99e50
Haseeb467 <trickyboy467@gmail.com>
2025-10-06 11:35:15 +0500

    add timout in nodemailer delete sg

--- services/nodemailerService.js
+++ services/nodemailerService.js
@@ -3,7 +3,6 @@ import path from 'path';
 import axios from 'axios';
 import validator from 'validator';
 import nodemailer from 'nodemailer';
-// import sgTransport from "nodemailer-sendgrid";
 
 /**
  * Create Nodemailer transporter
